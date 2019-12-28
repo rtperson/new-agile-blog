a4de9016de6bbad04a5d6d358d3a7c0465cb7e36
Roger Turnau <rturnau@gmail.com>
2019-12-27 21:28:40 -0500

    fixed line endings

--- jake.sh
+++ jake.sh
@@ -1,1 +1,5 @@
-#!/bin/sh[ ! -f node_modules/.bin/jake ] && npm rebuildchmod 755 ./node_modules/.bin/*export PATH=$PATH:./node_modules/.binnode_modules/.bin/jake $*
+#!/bin/sh
+[ ! -f node_modules/.bin/jake ] && npm rebuild
+chmod 755 ./node_modules/.bin/*
+export PATH=$PATH:./node_modules/.bin
+node_modules/.bin/jake $*
