65f4c65aa579722ae99f2813240e5659bec81890
Roger Turnau <rturnau@gmail.com>
2021-03-09 16:12:53 -0500

    tested build on Mac. Now works for Chrome and FF

--- Jakefile.js
+++ Jakefile.js
@@ -69,7 +69,7 @@ task(
 desc("start-server");
 task("start-server", ["lint", "nodeVersion", "compile-ts"], async () => {
     jake.exec(
-        "npx ts-node src/server/server.ts",
+        "npx ts-node -e 'require(\"./src/server/server.ts\").startServer()'",
         () => {
             console.log("server started from jake");
         },
@@ -104,7 +104,7 @@ task(
                     },
                 }).then(() => {
                     jake.exec(
-                        "npx ts-node -e 'require(\"src/server/server.ts\").stopServer()'",
+                        "npx ts-node -e 'require(\"./src/server/server.ts\").stopServer()'",
                         () => {
                             console.log("server stopped from jake");
                             process.exit();
--- src/server/server.ts
+++ src/server/server.ts
@@ -2,9 +2,14 @@ import { newApp } from "./app";
 import http from "http";
 
 const app = newApp();
-const server = http.createServer({}, app).listen(app.get("port"), () => {
-    console.log("listening on port " + app.get("port"));
-});
+const server = http.createServer({}, app);
+
+export function startServer(): number {
+    server.listen(app.get("port"), () => {
+        console.log("listening on port " + app.get("port"));
+    });
+    return 0;
+}
 
 export function stopServer(): number {
     server.close();
