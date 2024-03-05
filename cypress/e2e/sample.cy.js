75827427e85c927b926f6f849cec155ac7953a83
Roger Turnau <rturnau@gmail.com>
2024-03-05 14:23:57 -0500

    Attempted to correct Ajv ctor issue

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -1,5 +1,8 @@
-describe("My First Test", () => {
+import Ajv from "ajv";
 
+let ajv = new Ajv.default({ allErrors: true });
+
+describe("My First Test", () => {
     it("Can visit the local server", () => {
         cy.visit("http://localhost:8081");
     });
@@ -8,5 +11,4 @@ describe("My First Test", () => {
         cy.visit("http://localhost:8081");
         cy.get(".title").should("contain", "Gathering Agility");
     });
-
 });
